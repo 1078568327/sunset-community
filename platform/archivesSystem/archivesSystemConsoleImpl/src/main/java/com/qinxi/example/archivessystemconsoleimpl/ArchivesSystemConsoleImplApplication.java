af80856ad42a76cbc2fc8bf52947c4bc5c950501
chenqinxi <1078568327@qq.com>
2021-02-18 17:39:19 +0800

    项目搭建

---
+++ platform/archivesSystem/archivesSystemConsoleImpl/src/main/java/com/qinxi/example/archivessystemconsoleimpl/ArchivesSystemConsoleImplApplication.java
@@ -0,0 +1,13 @@
+package com.qinxi.example.archivessystemconsoleimpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchivesSystemConsoleImplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchivesSystemConsoleImplApplication.class, args);
+    }
+
+}
---
+++ platform/archivesSystem/archivesSystemImpl/src/main/java/com/qinxi/example/archivessystemimpl/ArchivesSystemImplApplication.java
@@ -0,0 +1,13 @@
+package com.qinxi.example.archivessystemimpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchivesSystemImplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchivesSystemImplApplication.class, args);
+    }
+
+}
